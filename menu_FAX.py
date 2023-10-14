563e318dcfce82e0255bdc1eb36cacebb1941587
kindkorea <kindkorea@gmail.com>
2023-10-14 14:54:27 +0900

    팩스폴더 이벤트 확인

--- menu_FAX.py
+++ menu_FAX.py
@@ -17,61 +17,36 @@ class CustomHandler(FileSystemEventHandler):
     def on_modified(self, event): self.app.notify(event)
     def on_moved(self, event): self.app.notify(event)
 
-class WatchDirectory(object):
-    def __init__(self,src_path):
-        path = src_path
-        handler = CustomHandler(self)
-        self.observer = Observer()
-        self.observer.schedule(handler, path, recursive=True)
-        self.queue = Queue()
-        self.observer.start()
-        # self.list_box = list_box
-
-    def handle_watchdog_event(self, event):
-        """Called when watchdog posts an event"""
-        watchdog_event = self.queue.get()
-        print("event type:", type(watchdog_event))
-
-    def shutdown(self, event):
-        print("""Perform safe shutdown when GUI has been destroyed""")
-        self.observer.stop()
-        self.observer.join()
-
-    def notify(self, event):
-        """Forward events from watchdog to GUI"""
-        self.queue.put(event)
-        self.handle_watchdog_event(event)
-
-
 class MenuFax():
 
     def __init__(self, parent):
-        # tkinter.Tk.__init__(self,parent)
 
-        self.DIRECTOR_PATH = 'f:/OONGCHEON/사무실서류/수신팩스/'
-        self.frame = parent
-        self.parent = tkinter.Frame(self.frame)
+
+        self.DIRECTOR_PATH = 'f:\OONGCHEON\사무실서류\수신팩스\/'
+        self.parent = tkinter.Frame(parent)
         self.initialize()
         self.src_file =''
         self.src_path = self.DIRECTOR_PATH
         self.parent.pack(side='top')
-        watch = WatchDirectory(self.DIRECTOR_PATH)
-        # watch.mainloop()
+
+        # Observer 생성
+        handler = CustomHandler(self)
+        self.observer = Observer()
+        self.observer.schedule(handler, self.DIRECTOR_PATH, recursive=True)
+        self.queue = Queue()
+        self.observer.start()
+
+    
 
     def initialize(self):
-            
-        # self.window=tkinter.Tk()
-        # 윈도우창 설정
-        
-        # self.parent.title("웅천목재 프로그램")
-        # self.parent.geometry("640x400+100+100")
-        # self.parent.resizable(False, False)
+
         self.txt_value_entry = tkinter.StringVar()
-        # 프레임 1
-        self.frame=tkinter.Frame(self.parent, 
+        # 프레임1
+        self.frame=tkinter.LabelFrame(self.parent, 
                             width=300,
+                            text='FAX'
                             )
-        # 파일 리스트 박스 
+      
         self.scrollbar=tkinter.Scrollbar(self.frame)
         self.scrollbar.pack(side="right", fill="y")
 
@@ -83,9 +58,7 @@ class MenuFax():
                                 )
 
      
-        #원클릭 이벤트 설정
         self.listbox.bind('<<ListboxSelect>>', self.items_selected)
-        #더블클릭 이벤트 설정
         self.listbox.bind('<Double-Button-1>', self.items_doubleClicked)
 
 
@@ -95,14 +68,14 @@ class MenuFax():
         self.chk_active = tkinter.IntVar()
 
 
-        # 프레임 2
+        # ������ 2
         self.frame2 =tkinter.Frame(self.parent, 
                             width=300,
                             )
 
         self.btn_all_files = tkinter.Button(self.frame2, text='모든파일' ,command=self.all_files)
-        self.btn_noCheck_files = tkinter.Button(self.frame2, text='미확인파일', command=self.noCheck_files)
-        self.btn_Checked_files = tkinter.Button(self.frame2, text='확인파일', command=self.checked_files)
+        self.btn_noCheck_files = tkinter.Button(self.frame2, text='미확인', command=self.noCheck_files)
+        self.btn_Checked_files = tkinter.Button(self.frame2, text='확인', command=self.checked_files)
 
 
         self.frame3 =tkinter.Frame(self.parent, 
@@ -111,7 +84,7 @@ class MenuFax():
 
 
         self.chk_button = tkinter.Checkbutton(self.frame3,text='확인' , variable=self.chk_active)
-        self.btn_convert = tkinter.Button(self.frame3,text='이름바꾸기', command=self.convert_filename)
+        self.btn_convert = tkinter.Button(self.frame3,text='이름변환', command=self.convert_filename)
         self.company_name = tkinter.Entry(self.frame3,textvariable=self.txt_value_entry)
 
 
@@ -130,12 +103,15 @@ class MenuFax():
         self.noCheck_files()
 
     # print(f'{chk_active.get()}')
-        
-    def reload_file(self,filename):
+    
+    def event_handler(self):
+        print('event_handler')
+
+    def check_v_file(self,filename):
         self.company_name.delete(0,'end')
         self.company_name.insert(0,filename.split('_')[0])  
+        # self.txt_value_entry = 'hello'
 
-        self.txt_value_entry = 'hello'
         if filename[0] == 'v' :
             self.chk_active.set(True)
             return True
@@ -144,37 +120,49 @@ class MenuFax():
             return False
 
     def items_selected(self,event):
-        # print(f'{self.listbox.curselection()=}')
-        selected_indices = self.listbox.curselection()
-        if selected_indices:
-            index = selected_indices[0]
-            self.src_file = self.listbox.get(index)
-            self.reload_file(os.path.basename(self.src_file))
-        else:
-            print("No entry") 
+        if not event.widget.curselection():
+            return
+        selected_indices = self.listbox.curselection()[0]
+        self.src_file = self.listbox.get(selected_indices)
+        self.check_v_file(os.path.basename(self.src_file))
+
+        print(self.src_file)
+        # print(selected_indices)
+        # if selected_indices:
+        #     # index = selected_indices[0]
+        #     self.src_file = self.listbox.get(selected_indices)
+        #     self.reload_file(os.path.basename(self.src_file))
+        #     # self.company_name.select_adjust(tkinter.END)
+        # else:
+        #     print("No entry") 
     
 
     def items_doubleClicked(self,event):
-        selected_indices = self.listbox.curselection()
-        msg = self.listbox.get(selected_indices[0])
+        if not event.widget.curselection():
+            return
+        print(tkinter.Event)
+        # selected_indices = self.listbox.curselection()
+        # msg = self.listbox.get(selected_indices[0])
         # os.system(f'./img/{msg}')
 
 
 
     def FAX_rename(self, dst_file, is_checked) :
-            print(f'{self.src_file=}, {dst_file=}, {is_checked=}')
+        
             try :
-                #파일 생성일자를 업체명 뒤에 붙임
-                creation_time = time.gmtime(os.path.getctime(self.src_file))
-                file_ctime = time.strftime("%Y_%m_%d", creation_time)
+                # creation_time = time.gmtime(os.path.getctime(self.src_file))
+                file_ctime = time.strftime("%Y-%m-%d", time.gmtime(os.path.getctime(self.src_file)))
 
-                #소스 파일을 파일, 확장자로 쪼갬
                 file_path, file_name_ext = os.path.split(self.src_file)
                 file_name , file_ext = os.path.splitext(file_name_ext)
 
-                #새로 변경할 파일명을 조립함
+                if is_checked :
+                    dst_file_name = f'v{dst_file}'
+                else : 
+                    dst_file_name = dst_file.lstrip('v')
+                
                 try :
-                    dst_file_name = f'{file_path}/{dst_file}_{file_ctime}{file_ext}'
+                    dst_file_name = f'{file_path}/{dst_file_name}_{file_ctime}{file_ext}'
                     os.rename(self.src_file,dst_file_name)
                     self.noCheck_files()
                 except FileExistsError :
@@ -200,10 +188,18 @@ class MenuFax():
 
     def convert_filename(self):
         self.FAX_rename( self.company_name.get(), self.chk_active.get())
-        # print(f'{self.src_file}')
-        # print(f'{self.company_name.get()}')
-        # print(f'{self.chk_active.get()}')
- 
-# root = tkinter.Tk()
-# app = MenuFax(root) 
-# root.mainloop()
+
+    def handle_watchdog_event(self, event):
+        """Called when watchdog posts an event"""
+        watchdog_event = self.queue.get()
+        print("event type:", watchdog_event)
+
+    def shutdown(self, event):
+        print("""Perform safe shutdown when GUI has been destroyed""")
+        self.observer.stop()
+        self.observer.join()
+
+    def notify(self, event):
+        """Forward events from watchdog to GUI"""
+        self.queue.put(event)
+        self.handle_watchdog_event(event)
